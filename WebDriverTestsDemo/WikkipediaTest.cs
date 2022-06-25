d803441bf706c5fa6849df07e38892cc9526bac0
Rossen <Rossen@DESKTOP-9DC4VQ5>
2022-06-25 21:02:15 +0300

    Add project files.

---
+++ WebDriverTestsDemo/WikkipediaTest.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+
+using OpenQA.Selenium.Chrome;
+
+//create new cgrome browser instance
+var driver = new ChromeDriver();
+
+//navigate to Wikipedia
+driver.Url = "https://wikipedia.org";
+
+System.Console.WriteLine("CURRENT TITLE: " + driver.Title);
+
+//locate search button(FIELD) by ID
+var searchField = driver.FindElement(By.Id("searchInput"));
+
+//click on element
+searchField.Click();
+
+//type QA and press ENTER button on keyboard
+searchField.SendKeys("QA" + Keys.Enter);
+
+System.Console.WriteLine("TITLE AFTER SEARCH: " +driver.Title);
+
+//close browser
+driver.Quit();
+
+
+
